d87675a52658f6b171bb5e77a7dc80183a69763a
Vadim Korolev <vkorolev1506@gmail.com>
2021-02-24 19:21:05 +0300

    create basic strapi application

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7d8946156ee2646b6277a8696a1b1e4f'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd1744d30-465d-4203-b5ea-9132cd841878'
+};
