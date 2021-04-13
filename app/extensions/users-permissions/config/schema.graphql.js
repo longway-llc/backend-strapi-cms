c508fc27d2ff1b82ba42a33ca080f10b146ed92e
Vadim Korolev <vkorolev1506@gmail.com>
2021-04-13 20:28:03 +0300

    dev: add cart to 'me' gql schema

--- app/extensions/users-permissions/config/schema.graphql.js
+++ app/extensions/users-permissions/config/schema.graphql.js
@@ -2,11 +2,13 @@ module.exports = {
   definition: `
     extend type UsersPermissionsMe {
       user: UsersPermissionsUser,
+      cart: Cart
     }
   `,
   resolver: {
     UsersPermissionsMe: {
       user: user => user,
+      cart: cart => cart
     },
   }
 }
