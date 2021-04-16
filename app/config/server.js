387220446c1f2696140819ac213fb785212187a6
Vadim Korolev <vkorolev1506@gmail.com>
2021-04-16 19:24:36 +0300

    add deploy url

--- app/config/server.js
+++ app/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', undefined),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '7d8946156ee2646b6277a8696a1b1e4f'),
