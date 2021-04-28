b362bcf19351e0f5e1ad7514911d082d8e57b3c5
Vadim Korolev <vkorolev1506@gmail.com>
2021-04-28 19:21:50 +0300

    dev: add notification on new user create

--- app/extensions/users-permissions/models/User.js
+++ app/extensions/users-permissions/models/User.js
@@ -5,6 +5,15 @@ module.exports = {
       const {id} = data
       const cart = await strapi.services.cart.create()
       await strapi.query('user', 'users-permissions').update({id}, {cart: cart.id})
+
+      await strapi.plugins['email'].services.email.send({
+        to: 'admin@lwaero.net',
+        from: 'system@lwaero.net',
+        subject: `Новый пользователь на сайте: ${data.email}`,
+        text: `
+              На сайте авторизовался новый пользователь. Email: ${data.email}
+            `,
+      })
     },
   },
 };
