0263bdaadf343c191348aa650de2c37522eb1ab1
Vadim Korolev <vkorolev1506@gmail.com>
2021-04-28 19:20:26 +0300

    remove publish condition

--- app/api/product/models/product.js
+++ app/api/product/models/product.js
@@ -12,11 +12,7 @@ module.exports = {
       strapi.services.algolia.saveObject(result, index);
     },
     afterUpdate(result, params, data) {
-      if (result.published_at) {
         strapi.services.algolia.saveObject(result, index);
-      } else {
-        strapi.services.algolia.deleteObject(result.id, index);
-      }
     },
     afterDelete(result, params) {
       strapi.services.algolia.deleteObject(result.id, index);
