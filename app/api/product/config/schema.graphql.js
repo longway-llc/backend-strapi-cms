f94a79654818a6e3797d848fa8b377dce27fbde7
Vadim Korolev <vkorolev1506@gmail.com>
2021-04-28 19:20:08 +0300

    update models

---
+++ app/api/product/config/schema.graphql.js
@@ -0,0 +1,39 @@
+module.exports = {
+  resolver: {
+    Mutation: {
+      requestQuote: {
+        description: 'отправляет на почту запрос КП',
+        policies: ['plugins::users-permissions.isAuthenticated'],
+        resolverOf: 'application::product.product.requestQuote',
+        resolver: async (_, {id}, {context}) => {
+          const product = await strapi.services.product.findOne({id})
+          console.log(product)
+
+          await strapi.plugins['email'].services.email.send({
+            to: context.state.user.email,
+            from: 'system@lwaero.net',
+            subject: `Request on ${product.pn}`,
+            text: `
+              Hello! You left a request for a cost of ${product.pn} on lwaero.net. Our manager contact with you at shotley time!
+            `,
+          })
+
+          await strapi.plugins['email'].services.email.send({
+            to: 'admin@lwaero.net',
+            from: 'system@lwaero.net',
+            subject: `Запрос КП на ${product.pn} от ${context.state.user.email}`,
+            text: `
+              Пользователь сайта: ${context.state.user.email} запросил КП на товар ${product.pn} ${product.uom}:
+              https://lwaero.net/products/${product.id}
+            `,
+          })
+          return 'Request successful created'
+        }
+      },
+    },
+  },
+  mutation: `
+    requestQuote(id: ID!): String,
+  `,
+}
+
